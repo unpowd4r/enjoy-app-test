2dfd19832453be3cb7f5ddd81ec62a6ef696ce65
unpowd4r <sergay.frolov@yandex.ru>
2026-02-16 15:30:02 +0400

    fix(auth): update tests for token-based authentication

--- src/shared/lib/__tests__/auth.test.ts
+++ src/shared/lib/__tests__/auth.test.ts
@@ -1,17 +1,20 @@
 import { isAuthenticated } from '../auth';
 
 describe('formatDate', () => {
-  it('should return correct value when LS is empty', () => {
+   it('should return false when token is not in LS', () => {
     expect(isAuthenticated()).toBeFalsy();
   });
 
-  it('should return correct value when LS have isAuth: true', () => {
-    localStorage.setItem('isAuth', 'true');
+  it('should return true when token exists in LS', () => {
+    localStorage.setItem('token', '1234567890');
     expect(isAuthenticated()).toBeTruthy();
   });
 
-  it('should return correct value when LS have isAuth: false', () => {
-    localStorage.setItem('isAuth', 'false');
+  it('should return false after token is removed', () => {
+    localStorage.setItem('token', '1234567890');
+    expect(isAuthenticated()).toBeTruthy();
+    
+    localStorage.removeItem('token');
     expect(isAuthenticated()).toBeFalsy();
   });
 });
