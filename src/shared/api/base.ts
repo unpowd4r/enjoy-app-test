b0d2575e6a7aa0ca4d8d2e110b71a85687e3d736
unpowd4r <sergay.frolov@yandex.ru>
2026-02-16 15:02:20 +0400

    feat(api): add error handling interceptor

--- src/shared/api/base.ts
+++ src/shared/api/base.ts
@@ -3,5 +3,17 @@ import axios from 'axios';
 export const API_URL = process.env.API_URL;
 
 export const api = axios.create({
-  baseURL: API_URL
+  baseURL: API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const errorMessage = error.response?.data?.message || error.message;
+    
+    return Promise.reject(new Error(errorMessage));
+  }
+);
